3a8b8d4adea6e31b2eb9d4a22b65284d3699ce4b
Olivier Mac 2024 <olgil12@ulaval.ca>
2024-05-16 11:31:31 -0400

    plot shows filter band

--- loadRun.py
+++ loadRun.py
@@ -57,7 +57,7 @@ def loadRun(ra_dec, type="AGN", band="i"):
     symbol = "+" if ra_dec[1] > 0 else ""
     total_compare(flux_list_2d, label_list_2d, flux_list_1d, label_list_1d, deltaPix = fitting_run_result.fitting_specify_class.deltaPix,
                         zp=fitting_run_result.zp, if_annuli=False, arrows= False, show_plot = True, mask_image = fitting_run_result.fitting_specify_class.kwargs_likelihood['image_likelihood_mask_list'][0],
-                        target_ID = f'{str(ra_dec[0])+symbol+str(ra_dec[1])}', sum_rest = True)
+                        target_ID = f'{str(ra_dec[0])+symbol+str(ra_dec[1])}-{band}', sum_rest = True)
 
 
     fitting_run_result.fitting_specify_class.plot_fitting_sets()
