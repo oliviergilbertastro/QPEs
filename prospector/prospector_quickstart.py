99e6634ecfad4bb801004677b39ae7df584ca24b
Olivier Mac 2024 <olgil12@ulaval.ca>
2024-06-03 13:56:20 -0400

    changing directory like a pro

--- prospector/prospector_quickstart.py
+++ prospector/prospector_quickstart.py
@@ -1,3 +1,14 @@
+import os
+import sys
+
+#Little code to ensure we are in the user directory running this code so the env variable works correctly  
+home = os.getcwd()
+if home != "/Users/oliviergilbert":
+    os.system("cd ../../../ \nsource .bash_profile \npython3 /Users/oliviergilbert/Desktop/QPEs/QPEs/prospector/prospector_quickstart.py")
+    sys.exit()
+
+#Need to download fsps from https://github.com/cconroy20/fsps and put it locally, change the path in .bash_profile to point to it
+
 import fsps
 import dynesty
 import sedpy
