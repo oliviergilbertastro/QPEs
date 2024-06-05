4ce0c07789e2061961c834fff1fa317398e5f5d4
Olivier Mac 2024 <olgil12@ulaval.ca>
2024-06-05 16:33:32 -0400

    viewing filters

--- prospector/checkFilter.py
+++ prospector/checkFilter.py
@@ -1,28 +1,29 @@
-import os
-import sys
-
-#Little code to ensure we are in the user directory running this code so the env variable works correctly  
-#home = os.getcwd()
-#if home != "/Users/oliviergilbert":
-#    os.system("cd ../../../ \nsource .bash_profile \npython3 /Users/oliviergilbert/Desktop/QPEs/QPEs/prospector/checkFilter.py")
-#    sys.exit()
-#os.system("cd ../../../ \nsource .bash_profile")
-#os.system("source ../../../.bash_profile")
-#os.system("export SPS_HOME=/Users/oliviergilbert/Desktop/QPEs/fsps-master")
-os.environ["SPS_HOME"] = "/Users/oliviergilbert/Desktop/QPEs/fsps-master"
-import astropy.table
-import fsps
-import dynesty
-import sedpy
-import h5py, astropy
 import numpy as np
-import astroquery
 from sedpy.observate import load_filters
-from prospect.utils.obsutils import fix_obs
-
+import matplotlib.pyplot as plt
 
 
 bands = "ugriz"
 
 filters = load_filters([f"sdss_{b}0" for b in bands])
-print(filters[0])
+for filter in filters:
+    ax1 = plt.subplot(111)
+    ax1.plot(filter.wavelength, filter.transmission, label=filter.name)
+ax1.set_xlabel(r"Wavelength [$\AA$]", fontsize=16)
+ax1.set_ylabel(r"Transmission", fontsize=16)
+ax1.xaxis.set_tick_params(labelsize=15)
+ax1.yaxis.set_tick_params(labelsize=15)
+plt.legend(fontsize=14)
+plt.show()
+
+bands = ["H","J","Ks"]
+filters = load_filters([f"twomass_{b}" for b in bands])
+for filter in filters:
+    ax1 = plt.subplot(111)
+    ax1.plot(filter.wavelength, filter.transmission, label=filter.name)
+ax1.set_xlabel(r"Wavelength [$\AA$]", fontsize=16)
+ax1.set_ylabel(r"Transmission", fontsize=16)
+ax1.xaxis.set_tick_params(labelsize=15)
+ax1.yaxis.set_tick_params(labelsize=15)
+plt.legend(fontsize=14)
+plt.show()
