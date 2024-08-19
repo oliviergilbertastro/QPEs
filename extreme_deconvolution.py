0b3d5913f3df5492100c53e1271332607306c295
Olivier Mac 2024 <olgil12@ulaval.ca>
2024-08-19 12:40:07 -0400

    plotting the distributions of bh masses

--- extreme_deconvolution.py
+++ extreme_deconvolution.py
@@ -5,8 +5,8 @@ from astroML.density_estimation import XDGMM
 from astroML.plotting.tools import draw_ellipse
 
 
-
-if "example" == True:
+# example
+if False:
     # Sample the dataset. 
     # Here we use sample size = 400 in the example, 
     # which converges in shorter time, and gives reasonable result.
@@ -112,3 +112,145 @@ if "example" == True:
             ax[i].set_ylabel('$y$')
 
     plt.show()
+
+
+
+
+
+
+from utils import recombine_arrays
+import pandas as pd
+from paper_data import *
+import random
+
+# get black hole masses:
+# load QPE and TDE data
+qpe_load_0 = np.loadtxt("QPE_allRelevantData_0.txt")
+qpe_load_1 = np.loadtxt("QPE_allRelevantData_1.txt")
+qpe_load_2 = np.loadtxt("QPE_allRelevantData_2.txt")
+QPE_fullData = recombine_arrays(qpe_load_0,qpe_load_1,qpe_load_2)
+
+tde_load_0 = np.loadtxt("TDE_allRelevantData_0.txt")
+tde_load_1 = np.loadtxt("TDE_allRelevantData_1.txt")
+tde_load_2 = np.loadtxt("TDE_allRelevantData_2.txt")
+TDE_fullData = recombine_arrays(tde_load_0,tde_load_1,tde_load_2)
+TDE_fullData = TDE_fullData[:10,:,:]
+
+french_tde_load_0 = np.loadtxt("french_TDE_allRelevantData_0.txt")
+french_tde_load_1 = np.loadtxt("french_TDE_allRelevantData_1.txt")
+french_tde_load_2 = np.loadtxt("french_TDE_allRelevantData_2.txt")
+french_TDE_fullData = recombine_arrays(french_tde_load_0,french_tde_load_1,french_tde_load_2)
+french_TDE_fullData = french_TDE_fullData[:,:,:]
+
+
+# load reference catalog
+refCat = np.loadtxt(f"referenceCatalog_with_uncertainties_final_9bins.txt")
+indices = list(range(len(refCat)))
+random.shuffle(indices)
+print(len(refCat))
+print(indices[:1000])
+refCat = refCat[indices[:1000]]
+#fieldnames = [f"col_{i}" for i in range(refCat.shape[1])]
+#refCat = pd.read_csv(f"referenceCatalog_with_uncertainties_final_9bins.txt", delimiter=" ", header=None, names=fieldnames)
+
+QPE_stellar_masses, TDE_stellar_masses, french_TDE_stellar_masses = QPE_fullData[:,3,:], TDE_fullData[:,3,:], french_TDE_fullData[:,3,:]
+QPE_mBH, TDE_mBH, french_TDE_mBH = QPE_fullData[:,4,:], TDE_fullData[:,4,:], french_TDE_fullData[:,4,:]
+TDE_stellar_masses = np.concatenate((TDE_stellar_masses, french_TDE_stellar_masses))
+TDE_redshifts = np.concatenate((TDE_redshifts, french_TDE_redshifts))
+TDE_mBH = np.concatenate((TDE_mBH, french_TDE_mBH))
+ref_mBH = refCat[:,100:102]
+ref_redshifts = refCat[:,4]
+
+
+# define a function to plot all distributions in the same format
+def plot_distribution(sample_z, sample_mbh, err_mbh, host="QPE"):
+    index = ["QPE","TDE","ref"].index(host)
+    plt.errorbar(x=sample_z, y=sample_mbh, yerr=err_mbh, fmt=".", label=["QPE","TDE","ref"][index], color=["blue","red","grey"][index])
+
+    
+
+# plot true distribution
+plt.figure(figsize=(5, 3.75))
+plot_distribution(ref_redshifts, ref_mBH[:,0], ref_mBH[:,1], host="ref")
+plot_distribution(QPE_redshifts, QPE_mBH[:,0], [QPE_mBH[:,1],QPE_mBH[:,2]], host="QPE")
+plot_distribution(TDE_redshifts, TDE_mBH[:,0], [TDE_mBH[:,1],TDE_mBH[:,2]], host="TDE")
+
+plt.xlabel('$z$')
+plt.ylabel('$M_\mathrm{BH}$')
+plt.legend()
+plt.title("Distribution",fontsize=10)
+plt.show()
+
+
+
+# add noise to get the "observed" distribution
+dx = 0.2 + 0.5 * np.random.random(N)
+dy = 0.2 + 0.5 * np.random.random(N)
+
+x = x_true + np.random.normal(0, dx)
+y = y_true + np.random.normal(0, dy)
+
+# plot noisy distribution
+plot_distribution('Noisy Distribution', x, y)
+
+plt.show()
+
+# stack the results for computation
+X = np.vstack([x, y]).T
+Xerr = np.zeros(X.shape + X.shape[-1:])
+diag = np.arange(X.shape[-1])
+Xerr[:, diag, diag] = np.vstack([dx ** 2, dy ** 2]).T
+
+clf = XDGMM(n_components=10, max_iter=200)
+
+clf.fit(X, Xerr)
+sample = clf.sample(N)
+
+# plot noisy distribution
+plot_distribution('Extreme Deconvolution Resampling', sample[:, 0], sample[:, 1])
+plt.show()
+
+# Plot the results
+fig = plt.figure(figsize=(5, 3.75))
+fig.subplots_adjust(left=0.1, right=0.95,
+                    bottom=0.1, top=0.95,
+                    wspace=0.02, hspace=0.02)
+
+ax1 = fig.add_subplot(221)
+ax1.scatter(x_true, y_true, s=4, lw=0, c='k')
+
+ax2 = fig.add_subplot(222)
+ax2.scatter(x, y, s=4, lw=0, c='k')
+
+ax3 = fig.add_subplot(223)
+ax3.scatter(sample[:, 0], sample[:, 1], s=4, lw=0, c='k')
+
+
+
+titles = ["True Distribution", "Noisy Distribution",
+        "Extreme Deconvolution\n  resampling",
+        "Extreme Deconvolution\n  cluster locations"]
+
+ax = [ax1, ax2, ax3]
+
+for i in range(len(ax)):
+    ax[i].set_xlim(-1, 13)
+    ax[i].set_ylim(-6, 16)
+
+    ax[i].xaxis.set_major_locator(plt.MultipleLocator(4))
+    ax[i].yaxis.set_major_locator(plt.MultipleLocator(5))
+
+    ax[i].text(0.05, 0.95, titles[i],
+            ha='left', va='top', transform=ax[i].transAxes)
+
+    if i in (0, 1):
+        ax[i].xaxis.set_major_formatter(plt.NullFormatter())
+    else:
+        ax[i].set_xlabel('$x$')
+
+    if i in (1, 3):
+        ax[i].yaxis.set_major_formatter(plt.NullFormatter())
+    else:
+        ax[i].set_ylabel('$y$')
+
+plt.show()
