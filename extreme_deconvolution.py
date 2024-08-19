e1df7dd336e74342888cacb80a75da66b870cbdb
Olivier Mac 2024 <olgil12@ulaval.ca>
2024-08-19 11:36:46 -0400

    black hole masses with uncertainties

---
+++ extreme_deconvolution.py
@@ -0,0 +1,5 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from astroML.density_estimation import XDGMM
+from astroML.plotting.tools import draw_ellipse
---
+++ referenceCatalog_withUncertainties_modif.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+refCat = np.loadtxt("referenceCatalog_with_uncertainties.txt")
+
+# add a (B/T)g column
+
+# nevermind it's already there
